571adb0054861cb1dcf1a87405370b5df92daa5e
Brainon Queiroz <brainonqueiroz@gmail.com>
2022-06-28 00:13:38 -0300

    Req 8 OK

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -119,6 +119,35 @@ class Wallet extends React.Component {
           </label>
           <button type="button" onClick={ this.clickHandler }>Adicionar despesa</button>
         </form>
+        <table>
+          <th>
+            Descrição
+          </th>
+          <th>
+            Tag
+          </th>
+          <th>
+            Método de pagamento
+          </th>
+          <th>
+            Valor
+          </th>
+          <th>
+            Moeda
+          </th>
+          <th>
+            Câmbio utilizado
+          </th>
+          <th>
+            Valor convertido
+          </th>
+          <th>
+            Moeda de conversão
+          </th>
+          <th>
+            Editar/Excluir
+          </th>
+        </table>
       </div>
     );
   }
